180537f6dd9f27c1bad0cee8c1bc9687f9521681
Adriangerardo2005 <adriangerardo057@gmail.com>
2016-04-11 22:44:19 -0500

    Clases para la tienda en java

---
+++ Teoria/ProyectoTienda/Java_tiendita.java
@@ -0,0 +1,40 @@
+
+public class Tiendita{
+	
+	int catidad;
+	int precio;
+	int stock;
+	stringn Nombreproducto;
+	
+}
+
+public producto(){
+	Nombreproducto = "Galletas"
+	cantidad = 5
+	precio = 7
+}
+
+public producto(){
+	Nombreproducto = "Cerveza"
+	cantidad = 5
+	precio = 35
+}
+
+public producto(){
+	Nombreproducto = "Papas"
+	cantidad = 5
+	precio = 10
+}
+
+public producto(){
+	Nombreproducto = "Vasos"
+	cantidad = 5
+	precio = 5
+}
+
+public producto(){
+	Nombreproducto = "Agua"
+	cantidad = 5
+	precio = 10
+	}
+
---
+++ Teoria/ProyectoTienda/Super clase_ Constructor.java
@@ -0,0 +1,14 @@
+public class Contructor{
+	
+	int nombre;
+	int precio;
+}
+
+public class Ticket(){
+	
+	public static void main (string args [])}
+	Nobreproducto = new Galletas();
+	Nobreproducto = new Cerveza();
+	Nobreproducto = new Vasos();
+	Nobreproducto = new Agua();
+	Nobreproducto = new Papas();
